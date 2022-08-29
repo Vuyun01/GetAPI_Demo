5c9978335e6d97a033887fb755500797c108329a
Vuyun01 <vu12401it@gmail.com>
2022-08-29 16:05:03 +0700

    updated

--- lib/getdatas.dart
+++ lib/getdatas.dart
@@ -38,7 +38,7 @@ class DataRequest{
   // url: 'https://yummly2.p.rapidapi.com/feeds/list',
   // params: {limit: '24', start: '0'},
   // headers: {
-  //   'X-RapidAPI-Key': '3a262f2dcdmsha04556f142fa8dep1e3e8bjsn94b5ccd003a0',
+  //   'X-RapidAPI-Key': '',
   //   'X-RapidAPI-Host': 'yummly2.p.rapidapi.com'
   // }
 
@@ -49,7 +49,7 @@ class DataRequest{
       'limit': '6', 'start' : '0'
     });
     var response = await http.get(url, headers: {
-      'X-RapidAPI-Key': '3a262f2dcdmsha04556f142fa8dep1e3e8bjsn94b5ccd003a0',
+      'X-RapidAPI-Key': 'your key',
       'X-RapidAPI-Host': 'yummly2.p.rapidapi.com'
     });
     if(response.statusCode == 200){
